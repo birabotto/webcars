24cb2731f88cf485818ff6d355915bfdb2d03632
Bira Botto <tibira.botto@gmail.com>
2024-08-19 17:15:26 -0400

    update users

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useContext } from 'react'
 import logoImg from '../../assets/logo.svg'
 import { Link, useNavigate } from 'react-router-dom'
 import { Container } from '../../components/container'
@@ -10,6 +10,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 
 import { auth } from '../../services/firebaseConnection'
 import { createUserWithEmailAndPassword, updateProfile, signOut } from 'firebase/auth'
+import { AuthContext } from '../../contexts/AuthContext'
 
 const schema = z.object({
   name: z.string().nonempty("O campo nome é obrigatório"),
@@ -20,6 +21,8 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>
 
 export function Register() {
+  const { handleInfoUser } = useContext(AuthContext);
+
   const navigate = useNavigate();
   const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
     resolver: zodResolver(schema),
@@ -43,6 +46,12 @@ export function Register() {
         displayName: data.name
       })
 
+      handleInfoUser({
+        name: data.name,
+        email: data.email,
+        uid: user.user.uid
+      })
+      
       console.log("CADASTRADO COM SUCESSO!")
       navigate("/dashboard", { replace: true })
 

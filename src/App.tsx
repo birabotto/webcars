a250270c0a020c768a57afa1c34149c4aa662cdc
Bira Botto <tibira.botto@gmail.com>
2024-08-19 17:14:19 -0400

    Protect routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,52 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import { Dashboard } from "./pages/dashboard";
+import { New } from "./pages/dashboard/new";
+import { CarDetail } from "./pages/car";
 
-import { createBrowserRouter } from 'react-router-dom'
-import { Home } from './pages/home'
-import { Login } from './pages/login'
-import { Register } from './pages/register'
-import { Dashboard } from './pages/dashboard'
-import { New } from './pages/dashboard/new'
-import { CarDetail } from './pages/car'
-
-import { Layout } from './components/layout'
+import { Layout } from "./components/layout";
+import { Private } from "./routes/Private";
 
 const router = createBrowserRouter([
   {
-    element: <Layout/>,
+    element: <Layout />,
     children: [
       {
         path: "/",
-        element: <Home/>
+        element: <Home />,
       },
       {
         path: "/car/:id",
-        element: <CarDetail/>
+        element: <CarDetail />,
       },
       {
         path: "/dashboard",
-        element: <Dashboard/>
+        element: (
+          <Private>
+            <Dashboard />
+          </Private>
+        ),
       },
       {
         path: "/dashboard/new",
-        element: <New/>
-      }
-    ]
+        element: (
+          <Private>
+            <New />
+          </Private>
+        ),
+      },
+    ],
   },
   {
     path: "/login",
-    element: <Login/>
+    element: <Login />,
   },
   {
     path: "/register",
-    element: <Register/>
-  }
-])
+    element: <Register />,
+  },
+]);
 
 export { router };
